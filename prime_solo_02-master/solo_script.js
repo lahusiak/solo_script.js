e6aa67aec7f5421fc01f25de7ceceda3d6376c5a
lahusiak <lahusiak@gmail.com>
2015-10-15 11:18:20 -0500

    Solo Object Challenge

--- prime_solo_02-master/solo_script.js
+++ prime_solo_02-master/solo_script.js
@@ -1,12 +1,19 @@
 // ! ! !
 // Three Bugs
 
-var arrayAtticus = ["Atticus", "2405", "47000", 3];
-var arrayJem = ["Jem", "62347", "63500", 4];
-var arrayBoo = ["Boo", "11435", "54000", 3];
-var arrayScout = ["Scout", "6243", "74750", 5];
+//var arrayAtticus = ["Atticus", "2405", "47000", 3];
+//var arrayJem = ["Jem", "62347", "63500", 4];
+//var arrayBoo = ["Boo", "11435", "54000", 3];
+//var arrayScout = ["Scout", "6243", "74750", 5];
 
-var array = [arrayAtticus, arrayJem, arrayBoo, arrayScout];
+var objectAtticus = {name: "Atticus", number: "2405", salary: 47000, score: 3};
+var objectJem = {name: "Jem", number: "62347", salary: 63500, score: 4};
+var objectBoo = {name: "Boo", number: "11435", salary: 54000, score: 3};
+var objectScout = {name: "Scout", number: "6243", salary: 74750, score: 5};
+
+
+
+var array = [objectAtticus, objectJem, objectBoo, objectScout];
 
 //Create variables used to write to the DOM
 var newEl, newText, position;
@@ -16,34 +23,52 @@ position = document.getElementById('content');
 //Loop the array, extracting each array and writing information to the DOM
 //Note that the information is not 'clean'
 for(var i = 0; i < array.length; i++){
-	array[i] = calculateSTI(array[i]);//added [i] to array;
- 	newEl = document.createElement('li');
+	array[i] = calculateSTI(array[i]);
+ 
+  //added [i] to array; asking yourself, what is this? A good place to start is console.log the first piece of logic. 
+ 	//could try console.log array[i], to start figuring this out. Forming specific questions. How do I think this thing should work?
+  newEl = document.createElement('li');
 	newText = document.createTextNode(array[i]);
 	newEl.appendChild(newText);
 	position.appendChild(newEl);
 }
 
-function calculateSTI(array){
-  var newArray = [];
+function calculateSTI(object){
+  //var newArray = [];
+  var newObject = {};
+  
+  newObject.name = object.name 
 
-  newArray[0] = array[0];
+  //console.log(object.name);
 
-  var employeeNumber = array[1];
-  var baseSalary = array[2];
-  var reviewScore = array[3];
+  //newArray[0] = array[0];
+
+  var employeeNumber = object.number;
+  var baseSalary = object.salary;
+  var reviewScore = object.score;
+
+  //console.log("This is baseSalary: ", baseSalary);
 
   var bonus = getBaseSTI(reviewScore) + getYearAdjustment(employeeNumber) - getIncomeAdjustment(baseSalary);
+  
+  //var bonus = getBaseSTI(object.score) + getYearAdjustment(object.number) - getIncomeAdjustment(object.Salary);
   
   if(bonus > 0.13){
     bonus = 0.13;
   }
 
-  newArray[1] = bonus;
-  newArray[2] = Math.round(baseSalary * (1.0 + bonus));
-  newArray[3] = baseSalary * bonus;
+  //newArray[1] = bonus;
+  //newArray[2] = Math.round(baseSalary * (1.0 + bonus));//added Math.round
+  //newArray[3] = baseSalary * bonus;
   
-  console.log(newArray[0] + " " + newArray[1] + " " + newArray[2] + " " + newArray[3]);
-  return newArray;
+  newObject.bonusPercent = bonus;
+  //console.log("This is newObject.bonusPercent: ", newObject.bonusPercent);
+  newObject.salaryWithBonus = Math.round(baseSalary * (1.0 + bonus));
+  //console.log("This is newObject.salaryWithBonus: ", newObject.salaryWithBonus);
+  newObject.salaryTimesBonus = baseSalary * bonus;
+
+  console.log(newObject.name + " " + newObject.bonusPercent + " " + newObject.salaryWithBonus + " " + newObject.salaryTimesBonus);
+  return newObject;
 }
 
 function getBaseSTI(reviewScore){
@@ -77,7 +102,7 @@ function getYearAdjustment(employeeNumber){
 }
 
 function getIncomeAdjustment(salary){
-  var incomeAdjustment = Math.round(0);//added Math.round
+  var incomeAdjustment = 0;
   salary = parseInt(salary);
   if(salary > 65000){
     incomeAdjustment = 0.01;
